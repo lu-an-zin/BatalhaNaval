f57d13d5eea65d57cff35d0d009dacf2617669d4
lu-an-zin <160800031+lu-an-zin@users.noreply.github.com>
2025-06-01 22:29:57 +0000

    Desafio: nível aventureiro

--- BatalhaNaval.c
+++ BatalhaNaval.c
@@ -5,14 +5,28 @@ int main(){
         //declarando a matriz
     int tabuleiro[10][10]= {0};
 
+    //navio 1 na horizontal
     for (int j = 2; j < 5; j++) {
         tabuleiro[2][j] = 3;
     }
 
-    for (int j = 7; j < 10; j++) {
+    //navio 2 na vertical
+
+    for (int j = 3; j < 6; j++) {
         tabuleiro[j][7] = 3;
     }
 
+    // navio 3 na diagonal
+    for (int i = 4; i < 7; i++) {
+        tabuleiro[i][i] = 3;
+    }
+
+    // navio 4 na diagonal
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[i][9 - i] = 3;
+    }
+
+    // print da matriz
     for(int i=0; i <10; i++){
         for(int j=0; j<10; j++){
             printf("%d ", tabuleiro[i][j]);
